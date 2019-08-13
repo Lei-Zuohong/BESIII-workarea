1e05517144b03a328441588ab12dd52813e4cbc4
Leizuohong <41337840+Lei-Zuohong@users.noreply.github.com>
2019-08-13 19:21:34 +0800

    123

--- OmegaAlg/OmegaAlg-00-00-02/OmegaAlg/Omega.h
+++ OmegaAlg/OmegaAlg-00-00-02/OmegaAlg/Omega.h
@@ -25,25 +25,6 @@ private:
   int m_checkDedx;
   int m_checkTof;
   double m_energy;
-  
-  NTuple::Tuple*  m_tuple1;          // charged track vertex
-  NTuple::Item<double>  m_vx0;
-  NTuple::Item<double>  m_vy0;
-  NTuple::Item<double>  m_vz0;
-  NTuple::Item<double>  m_vr0;
-  NTuple::Item<double>  m_rvxy0;
-  NTuple::Item<double>  m_rvz0;
-  NTuple::Item<double>  m_rvphi0;
-
-  NTuple::Tuple*  m_tuple2;          // fake photon
-  NTuple::Item<double>  m_dthe;
-  NTuple::Item<double>  m_dphi;
-  NTuple::Item<double>  m_dang;
-  NTuple::Item<double>  m_eraw;
-
-  NTuple::Tuple*  m_tuple3;          // Omega: raw mgg, etot
-  NTuple::Item<double>  m_m2gg;
-  NTuple::Item<double>  m_etot;
 
   NTuple::Tuple*  m_tuple4;          // Omega 4C
   NTuple::Item<double>  m_chi1;
@@ -63,61 +44,6 @@ private:
   NTuple::Item<double>  m_fcos;
   NTuple::Item<double>  m_elow;
 
-  NTuple::Tuple* m_tuple7;           // dE/dx
-  NTuple::Item<double> m_ptrk;
-  NTuple::Item<double> m_chie;
-  NTuple::Item<double> m_chimu;
-  NTuple::Item<double> m_chipi;
-  NTuple::Item<double> m_chik;
-  NTuple::Item<double> m_chip;
-  NTuple::Item<double> m_probPH;
-  NTuple::Item<double> m_normPH;
-  NTuple::Item<double> m_ghit;
-  NTuple::Item<double> m_thit;
-
-  NTuple::Tuple* m_tuple8;   // endcap tof
-  NTuple::Item<double> m_ptot_etof;
-  NTuple::Item<double> m_cntr_etof;
-  NTuple::Item<double> m_te_etof;
-  NTuple::Item<double> m_tmu_etof;
-  NTuple::Item<double> m_tpi_etof;
-  NTuple::Item<double> m_tk_etof;
-  NTuple::Item<double> m_tp_etof;
-  NTuple::Item<double> m_ph_etof;
-  NTuple::Item<double> m_rhit_etof;
-  NTuple::Item<double> m_qual_etof;
-
-  NTuple::Tuple* m_tuple9;  // barrel inner tof
-  NTuple::Item<double> m_ptot_btof1;
-  NTuple::Item<double> m_cntr_btof1;
-  NTuple::Item<double> m_te_btof1;
-  NTuple::Item<double> m_tmu_btof1;
-  NTuple::Item<double> m_tpi_btof1;
-  NTuple::Item<double> m_tk_btof1;
-  NTuple::Item<double> m_tp_btof1;
-  NTuple::Item<double> m_ph_btof1;
-  NTuple::Item<double> m_zhit_btof1;
-  NTuple::Item<double> m_qual_btof1;
-
-  NTuple::Tuple* m_tuple10;  // barrel outer tof
-  NTuple::Item<double> m_ptot_btof2;
-  NTuple::Item<double> m_cntr_btof2;
-  NTuple::Item<double> m_te_btof2;
-  NTuple::Item<double> m_tmu_btof2;
-  NTuple::Item<double> m_tpi_btof2;
-  NTuple::Item<double> m_tk_btof2;
-  NTuple::Item<double> m_tp_btof2;
-  NTuple::Item<double> m_ph_btof2;
-  NTuple::Item<double> m_zhit_btof2;
-  NTuple::Item<double> m_qual_btof2;
-  
-  NTuple::Tuple* m_tuple11;  // Particle ID info.
-  NTuple::Item<double> m_ptrk_pid;
-  NTuple::Item<double> m_cost_pid;
-  NTuple::Item<double> m_dedx_pid;
-  NTuple::Item<double> m_tof1_pid;
-  NTuple::Item<double> m_tof2_pid;
-  NTuple::Item<double> m_prob_pid;
 
 };
 
