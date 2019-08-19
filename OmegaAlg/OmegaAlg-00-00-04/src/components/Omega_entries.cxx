b0301d991cc3959cf8eb39a7b26709ee7b6ff85d
Leizuohong <41337840+Lei-Zuohong@users.noreply.github.com>
2019-08-19 15:45:03 +0800

    123

---
+++ OmegaAlg/OmegaAlg-00-00-04/src/components/Omega_entries.cxx
@@ -0,0 +1,9 @@
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "OmegaAlg/Omega.h"
+
+DECLARE_ALGORITHM_FACTORY( Omega )
+
+DECLARE_FACTORY_ENTRIES( OmegaAlg ) {
+  DECLARE_ALGORITHM(Omega);
+}
+
