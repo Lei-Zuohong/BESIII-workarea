111a45516cc775a059b36b87f6bce08709ecfb76
Leizuohong <41337840+Lei-Zuohong@users.noreply.github.com>
2019-08-19 09:39:25 +0800

    123

---
+++ OmegaAlg/OmegaAlg-00-00-03/OmegaAlg/Omega.h
@@ -0,0 +1,50 @@
+#ifndef Physics_Analysis_Omega_H
+#define Physics_Analysis_Omega_H 
+
+#include "GaudiKernel/AlgFactory.h"
+#include "GaudiKernel/Algorithm.h"
+#include "GaudiKernel/NTuple.h"
+//#include "VertexFit/ReadBeamParFromDb.h"
+
+class Omega : public Algorithm {
+public:
+  Omega(const std::string& name, ISvcLocator* pSvcLocator);
+  StatusCode initialize();
+  StatusCode execute();
+  StatusCode finalize();  
+
+private:
+  double m_vr0cut;
+  double m_vz0cut;
+  double m_energyThreshold;
+  double m_gammaPhiCut;
+  double m_gammaThetaCut;
+  double m_gammaAngleCut;
+  int m_test4C;
+  int m_test5C;
+  int m_checkDedx;
+  int m_checkTof;
+  double m_energy;
+
+  NTuple::Tuple*  m_tuple4;          // Omega 4C
+  NTuple::Item<double>  m_chi1;
+  NTuple::Item<double>  m_mpi0;
+
+  NTuple::Tuple*  m_tuple5;          // Omega 5C
+  NTuple::Item<double>  m_chi2;
+  //NTuple::Item<double>  m_mrh0;
+  //NTuple::Item<double>  m_mrhp;
+  //NTuple::Item<double>  m_mrhm;
+  NTuple::Item<double>  m_mpi01;
+  NTuple::Item<double>  m_mpi02;
+  NTuple::Item<double>  m_mpi03;
+  NTuple::Item<double>  m_momega;
+
+  NTuple::Tuple*  m_tuple6;          // photons
+  NTuple::Item<double>  m_fcos;
+  NTuple::Item<double>  m_elow;
+
+
+};
+
+#endif 
