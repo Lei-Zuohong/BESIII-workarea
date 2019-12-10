e5e0e8424b6456488bc9f1ab298b374c141b24eb
Leizuohong <lzh791397845@gmail.com>
2019-12-10 14:24:08 +0800

    123

---
+++ PmAlg/PmAlg-00-00-01/PmAlg/Pm.h
@@ -0,0 +1,30 @@
+#ifndef Physics_Analysis_Pm_H
+#define Physics_Analysis_Pm_H
+
+#include "GaudiKernel/AlgFactory.h"
+#include "GaudiKernel/Algorithm.h"
+#include "GaudiKernel/NTuple.h"
+//#include "VertexFit/ReadBeamParFromDb.h"
+
+class Pm : public Algorithm
+{
+public:
+  Pm(const std::string &name, ISvcLocator *pSvcLocator);
+  StatusCode initialize();
+  StatusCode execute();
+  StatusCode finalize();
+
+private:
+  int m_test4C;
+  double m_energy;
+
+  NTuple::Tuple *m_tuple4;
+  NTuple::Item<double> m_chisq_4c;
+  NTuple::Item<double> m_pi0;
+  NTuple::Item<double> m_omega;
+  NTuple::Item<double> m_omegapip;
+  NTuple::Item<double> m_oemgapim;
+  NTuple::Item<double> m_pipm;
+};
+
+#endif
